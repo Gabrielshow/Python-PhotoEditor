9e2ac5d26d8c4dc42a59c61fb46730ecf728a6d5
Gabrielshow <adefelashogbanmu@gmail.com>
2024-06-14 13:34:07 +0100

    caught minor bugs

--- photoEditor.py
+++ photoEditor.py
@@ -5,23 +5,23 @@ from sys import argv
 
 base_path = Path(argv[1]) if len(argv) > 1 else Path.cwd()
 
-input_dir = base_path\'imgs'
-output_dir = base_path\'editedImgs'
+input_dir = base_path/'imgs'
+output_dir = base_path/'editedImgs'
 output_dir.mkdir(parents=True, exist_ok=True)
 # fullpath = base_path + path
 # print(fullpath)
 
 try:
     if not os.path.isdir(input_dir):
-        print(f"Error: '{fullpath}' is not a valid directory.")
+        print(f"Error: '{input_dir}' is not a valid directory.")
     elif not os.listdir(input_dir):
-        print(f"Error: '{fullpath}' is empty or does not contain any image files.")
+        print(f"Error: '{input_dir}' is empty or does not contain any image files.")
     else:
         # Create the output directory if it doesn't exist
 #         output_dir = f"{base_path + pathOut}"
 #         os.makedirs(output_dir, exist_ok=True)
-        for filename in os.listdir(fullpath):
-            img_path = input_dir\filename
+        for filename in os.listdir(input_dir):
+            img_path = input_dir/filename
             with Image.open(img_path) as img:
                 edit = img.filter(ImageFilter.SHARPEN).convert('L').rotate(-90)
     
@@ -31,7 +31,7 @@ try:
     
                 clean_name = os.path.splitext(filename)[0]
     
-                output_file = output_dir\f"{clean_name}_edited.jpg"
+                output_file = output_dir/f"{clean_name}_edited.jpg"
                 print(f"Saving edited image to: {output_file}")
                 edit.save(output_file)
 except Exception as e:
